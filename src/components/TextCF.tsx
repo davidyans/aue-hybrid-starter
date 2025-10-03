c6e61b8e1a1b6150e9a44aa6c21ac4ae8829fd96
David Yanez <dyanez@oshyn.com>
2025-10-03 11:43:33 -0500

    text component fix

--- src/components/TextCF.tsx
+++ src/components/TextCF.tsx
@@ -19,12 +19,12 @@ export default function TextCF({
   const html = item.text?.html ?? "";
 
   return (
-    <div className="flex flex-wrap items-center bg-white rounded-lg shadow-md p-6 max-w-3xl w-full mb-5">
+    <div className="flex flex-wrap items-center bg-white rounded-lg shadow-md p-6 max-w-3xl w-full mb-5"
+      data-aue-resource={resource}
+      data-aue-type="component"
+      data-aue-model="textcf"
+      data-aue-label={label}>
       <div
-        data-aue-resource={resource}
-        data-aue-type="component"
-        data-aue-model="textcf"
-        data-aue-label={label}
         className="text-lg text-gray-700 w-full"
       >
         <Tag
